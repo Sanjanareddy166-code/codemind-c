c9965fe3a27021ecc2f21271d80858b3e20d99e8
Sanjanareddy166-code <99863393+Sanjanareddy166-code@users.noreply.github.com>
2022-02-23 19:37:26 +0530

    #include<stdio.h> int main() {     int n,rem,sum=0,product=1;     scanf("%d",&n);     while(n!=0)     {         rem=n%10;         sum=sum+rem;         product=product*rem;         n=n/10;     }     printf("%d",product-sum);     return 0; }

---
+++ Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,rem,sum=0,product=1;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        rem=n%10;
+        sum=sum+rem;
+        product=product*rem;
+        n=n/10;
+    }
+    printf("%d",product-sum);
+    return 0;
+}
